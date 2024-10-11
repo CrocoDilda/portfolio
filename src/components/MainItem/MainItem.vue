49229d0bf27c79686deff4bf86f3f1715f0c4b35
CrocoDilda <cucuprum4k@gmail.com>
2024-10-11 17:43:36 +0300

    made adaptive from section hero

--- src/components/MainItem/MainItem.vue
+++ src/components/MainItem/MainItem.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import HeroSection from "./Sections/HeroSecrion/HeroSection.vue"
 import TerminalSection from "./Sections/TerminalSection/TerminalSection.vue"
-// import SecondTerminal from "./Sections/SecondTerminal/SecondTerminal.vue"
 import ProjectsSection from "./Sections/ProjectsSection/ProjectsSection.vue"
 import ContactSection from "./Sections/ContactsSection/ContactSection.vue"
 </script>
